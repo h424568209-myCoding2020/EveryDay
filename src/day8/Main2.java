ca1ed07b06149ce0242e5a68e784bdc576fb3537
h424568209 <424568209@qq.com>
2020-03-12 16:32:32 +0800

    每日一题

--- src/day8/Main2.java
+++ src/day8/Main2.java
@@ -1,24 +1,48 @@
 package day8;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main2 {
     public  static int countNumberOf2s(int n) {
         // write code here
 
-        int count = 0, i;
-        if (n < 2) return 0;
-        else if (n <= 10) return 1;
-        else for (int j = 2; j <= n; j++) {
-                i = j;
-                while (i > 0) {
-                    if (i % 10 == 2) count++;
-                    i /= 10;
-                    if (i == 0) break;
-                }
+        int count = 0 ;
+        int factor = 1;
+        int low = 0;
+        int cur = 0;
+        int high = 0;
+        while(n/factor !=0){
+            low = n-(n/factor) *factor;
+            cur = (n/factor) %10;
+            high = n/(factor*10);
+            switch (cur){
+                case 0:
+                case 1:
+                    cur+=high * factor;
+                    break;
+                case 2:
+                    count += high * factor+low  +1;
+                    break;
+                    default:
+                        count+=(high +1 ) *factor;
+                        break;
             }
+            factor *= 10;
+        }
         return count;
+//
+//        int count = 0, i;
+//        if (n < 2) return 0;
+//        else if (n <= 10) return 1;
+//        else for (int j = 2; j <= n; j++) {
+//                i = j;
+//                while (i > 0) {
+//                    if (i % 10 == 2) count++;
+//                    i /= 10;
+//                    if (i == 0) break;
+//                }
+//            }
+//        return count;
 
 //
 //        int []arr = new int[n];
